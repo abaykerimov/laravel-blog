4d5fdb3a10c83afed1d92ac44b36522df51d93e2
abaykerimov <abay-001@mail.ru>
2018-11-07 21:01:15 +0600

    updated

--- resources/views/admin/posts/details.blade.php
+++ resources/views/admin/posts/details.blade.php
@@ -14,13 +14,34 @@
         </div>
     </div>
 @stop
+@push('styles')
+    <!-- Include CSS for icons. -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Include Editor style. -->
+    <link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/css/froala_style.min.css" rel="stylesheet" type="text/css" />
+
+@endpush
 @push('scripts')
-    <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=cgb46t2y39y00y9slf9cgs81l747j3rihvja55fjrv47w5po"></script>
+    {{--<script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=cgb46t2y39y00y9slf9cgs81l747j3rihvja55fjrv47w5po"></script>--}}
+    {{--<script>--}}
+        {{--tinymce.init({--}}
+            {{--selector:'textarea',--}}
+            {{--plugins: "a11ychecker, advcode, linkchecker, media mediaembed, powerpaste, tinydrive, tinymcespellchecker",--}}
+            {{--toolbar: "a11ycheck, code, insertfile"--}}
+        {{--});--}}
+    {{--</script>--}}
+
+    <!-- Include jQuery lib. -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include Editor JS files. -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@2.9.0/js/froala_editor.pkgd.min.js"></script>
+
+    <!-- Initialize the editor. -->
     <script>
-        tinymce.init({
-            selector:'textarea',
-            plugins: "a11ychecker, advcode, linkchecker, media mediaembed, powerpaste, tinydrive, tinymcespellchecker",
-            toolbar: "a11ycheck, code, insertfile"
+        $(function() {
+            $('textarea').froalaEditor()
         });
     </script>
 @endpush
